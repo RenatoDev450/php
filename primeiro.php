f404cdbb808df97d6f8e7530a09f52dfe4299cff
RenatoDev450 <renatooliveiramendes27@gmail.com>
2026-03-03 21:50:38 -0300

    novo arquivi modificaçao php

--- primeiro.php
+++ primeiro.php
@@ -11,3 +11,16 @@ echo "quarto";
 
 
  ?>
+ //commentario de linha
+
+ <?php 
+ $num1=10;
+ $num2=10,5;
+ $nome="Etecia";
+ $sobreNome="agostina"
+ $status = true;
+ $flag = false;
+ $resp =  $num1 +  $num2;
+ $resp = $nome + $sobreNome;
+
+  ?>
---
+++ segundo.php
@@ -0,0 +1,11 @@
+<?php 
+ $num1=10;
+ $num2=10.5;
+ $nome="Etecia";
+ $sobreNome="agostina";
+ $status = true;
+ $flag = false;
+ $resp =  $num1 +  $num2;
+ $resp = $nome + $sobreNome;
+
+?>
